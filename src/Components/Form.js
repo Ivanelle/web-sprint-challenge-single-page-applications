e68a54a8b3ae3a506b042d86c6c9a2f390214817
Ivanelle Hunt <ivanellep@gmail.com>
2023-09-04 11:40:16 -0700

    form has name text

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect} from 'react';
+
+const style = { margin: '1rem', padding: '1rem', border: '2px solid black' }
 
 const initialValues = {
 	name: '',
@@ -14,9 +17,22 @@ const initialErrors = {
 	name: ''
 }
 
+const onChange = () => {
+    
+}
 
+const onSubmit = () => {
 
+}
 
 export default function Form(props) {
-    return <h2>Build Your Own Pizza</h2>
-}
+const [values, setValues] = useState(initialValues);
+const [errors, setErrors] = useState(initialErrors);
+
+    return (
+    <div style={style}>
+        <h2>Build Your Own Pizza</h2>
+        <label htmlFor='name'>Insert Name:</label>
+        <input value={values.name} id ='name-input' onChange={onChange}type='text' name='name' placeholder='Your Pizzas Name'/>
+    </div>
+)}
